68764141bee2001d5613371eb0676634ce12c250
yingtingyang <yangy389@newschool.edu>
2017-05-15 06:16:04 -0400

    wtf

--- replace.js
+++ replace.js
@@ -10,6 +10,9 @@ function doScripts(context){
 	chrome.tabs.executeScript(null, {file:"box2d.js"}, function(){
 	chrome.tabs.executeScript(null, {code: "var doingBrowserAction = "+context+";"}, function(){
 	chrome.tabs.executeScript(null, {file: "myscript.js"}, function(){
+	chrome.tabs.executeScript(null, {file: "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"},function () {
+		// body...
+	});	
 							
 						});
 					});
